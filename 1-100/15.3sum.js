05e3ceb2a144bc499aa7407b2080dfbb8a6c961f
Anibal Español <mecanicodev@gmailcom>
2022-02-01 17:02:42 -0400

    Problem #15 performance results added

--- 1-100/15.3sum.js
+++ 1-100/15.3sum.js
@@ -80,3 +80,11 @@ var threeSum = function(nums) {
 // console.time("threeSum")
 // console.log(threeSum([34,55,79,28,46,33,2,48,31,-3,84,71,52,-3,93,15,21,-43,57,-6,86,56,94,74,83,-14,28,-66,46,-49,62,-11,43,65,77,12,47,61,26,1,13,29,55,-82,76,26,15,-29,36,-29,10,-70,69,17,49]))
 // console.timeEnd("threeSum")
+
+
+
+
+/*
+Runtime: 2876 ms, faster than 10.42% of JavaScript online submissions for 3Sum.
+Memory Usage: 71.7 MB, less than 5.43% of JavaScript online submissions for 3Sum.
+*/
